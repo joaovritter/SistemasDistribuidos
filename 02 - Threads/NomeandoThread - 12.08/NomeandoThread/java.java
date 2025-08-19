a9b9eb1b6a124e3c63c50b742b3e6c170e38fc6a
João Vitor dos Santos Ritter <125499584+joaovritter@users.noreply.github.com>
2025-08-19 19:37:01 -0300

    Add files via upload

---
+++ 02 - Threads/NomeandoThread - 12.08/NomeandoThread/java.java
@@ -0,0 +1,119 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class PopulaLista implements Runnable {  //uso de threads sem memória compartilhada
+    private List<Integer> lista;
+    Integer tamanho;
+
+    //construtor
+    public PopulaLista(List<Integer> lista, Integer tamanho) {
+        this.lista = lista;
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public void run() {
+        //aqui que está a tarefa ou rotina a ser concomitada
+        Thread t = Thread.currentThread();
+        System.out.println("Thread " + t.getName() + " iniciada para popular a lista com tamanho: " + tamanho);
+        Random random = new Random();
+        for (int i = 0; i < tamanho; i++) {
+            lista.add(random.nextInt(tamanho));
+        }
+        System.out.println("Thread finalizada " + t.getName());
+    }
+}
+
+
+class OrdenaBolha implements Runnable { //uso de threads sem memória compartilhada
+    private List<Integer> lista;
+
+    public OrdenaBolha(List<Integer> lista) {
+        this.lista = lista;
+    }
+    @Override
+    public void run() {
+        //aqui está a tarefa ou rotina a ser concomitada - no caso, bubble sort
+        boolean houveTroca;
+        int tmp;
+        Thread t = Thread.currentThread();
+        System.out.println("Thread " + t.getName() + " iniciada para ordenar com bolha a lista com tamanho: " + lista.size());
+        do {
+            houveTroca = false;
+            for (int i = 0; i < lista.size() - 1; i++) {
+                if (lista.get(i) > lista.get(i+1)) {
+                    houveTroca = true;
+                    tmp = lista.get(i);
+                    lista.set(i, lista.get(i+1));
+                    lista.set(i+1, tmp);
+                }
+            }
+        } while (houveTroca);
+    }
+}
+
+class OrdenaPente implements Runnable { //uso de threads sem memória compartilhada
+    private List<Integer> lista;
+
+    public OrdenaPente(List<Integer> lista) {
+        this.lista = lista;
+    }
+
+    public void run() {
+        //aqui está a tarefa ou rotina a ser concomitada - no caso, bubble sort
+        boolean houveTroca;
+        int tmp;
+        int distancia = lista.size();
+        Thread t = Thread.currentThread();
+        System.out.println("Thread " + t.getName() + " iniciada para ordenar com pente a lista com tamanho: " + lista.size());
+        do {
+            distancia = (int) (distancia / 1.3);
+            houveTroca = false;
+            for (int i = 0; i < lista.size() - distancia; i++) {
+                if (lista.get(i) > lista.get(i+distancia)) {
+                    houveTroca = true;
+                    tmp = lista.get(i);
+                    lista.set(i, lista.get(i+distancia));
+                    lista.set(i+distancia, tmp);
+                }
+            }
+        } while (distancia > 1 || houveTroca);
+    }
+}
+
+public class Java {
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> lista1 = new ArrayList<>();
+        List<Integer> lista2 = new ArrayList<>();
+        List<Integer> lista3 = new ArrayList<>();
+
+        Thread threadPopula1 = new Thread(new PopulaLista(lista1, 10000), "PopulaLista1");
+        Thread threadPopula2 = new Thread(new PopulaLista(lista2, 1000), "PopulaLista2");
+        Thread threadPopula3 = new Thread(new PopulaLista(lista3, 5000), "PopulaLista3");
+
+
+
+        threadPopula1.start();
+        threadPopula2.start();
+        threadPopula3.start();
+
+        threadPopula1.join();
+        threadPopula2.join();
+        threadPopula3.join();
+
+
+        Thread threadOrdena1 = new Thread(new OrdenaPente(lista1), "OrdenaPente1");
+        Thread threadOrdena2 = new Thread(new OrdenaBolha(lista2), "OrdenaBolha1");
+        Thread threadOrdena3 = new Thread(new OrdenaBolha(lista3), "OrdenaBolha2");
+
+        threadOrdena1.start();
+        threadOrdena2.start();
+        threadOrdena3.start();
+    }
+
+
+}
---
+++ 02 - Threads/NomeandoThread - 12.08/NomeandoThread/python1.py
@@ -0,0 +1,78 @@
+import threading
+import random
+import time
+ 
+def popular_lista(lista, tamanho_lista):
+    t = threading.current_thread()
+    print (f"Thread: {t.name} populando lista de tamanho {tamanho_lista}")
+    for _ in range(tamanho_lista):
+        lista.append( random.randint(1000,10000) )
+        
+    
+ 
+def ordenar_bolha(lista):
+    houve_troca = True
+    
+    t = threading.current_thread()
+    print (f"Thread: {t.name} ordenando lista de tamanho {len(lista)}")
+    
+    while houve_troca:
+        houve_troca = False
+        for i in range(len(lista) - 1):
+            if lista[i] > lista[i + 1]:
+                lista[i], lista[i + 1] = lista[i + 1], lista[i]
+                houve_troca = True
+                
+    print('Bolha finalizado...')
+ 
+def ordenar_pente(lista):    
+    n = len(lista)
+    distancia = n
+    fator = 1.3
+    houve_troca = True
+    
+    t = threading.current_thread()
+    print (f"Thread: {t.name} ordenando lista de tamanho {len(lista)}")
+ 
+    while distancia > 1 or houve_troca:
+        distancia = int(distancia / fator)
+        if distancia < 1:
+            distancia = 1
+        houve_troca = False
+ 
+        for i in range(n - distancia):
+            if lista[i] > lista[i + distancia]:
+                lista[i], lista[i + distancia] = lista[i + distancia], lista[i]
+                houve_troca = True
+    
+    print('Pente finalizado...')
+ 
+ 
+if __name__ == "__main__":
+    lista1 = []
+    lista2 = []
+    lista3 = []
+ 
+    
+    thread_popula_lista1 = threading.Thread(target=popular_lista,args=(lista1, 5000), name="PopulaLista1")
+    thread_popula_lista2 = threading.Thread(target=popular_lista,args=(lista2, 1000), name="PopulaLista2")
+    thread_popula_lista3 = threading.Thread(target=popular_lista,args=(lista3, 500), name="PopulaLista3")
+ 
+    thread_popula_lista1.start()
+    thread_popula_lista2.start()
+    thread_popula_lista3.start()
+    
+    thread_popula_lista1.join()
+    thread_popula_lista2.join()
+    thread_popula_lista3.join()
+ 
+    thread_ordena_pente = threading.Thread(target=ordenar_pente,args=(lista1,), name="OrdenaPente")
+    thread_ordena_bolha3 = threading.Thread(target=ordenar_bolha,args=(lista3,), name="OrdenaBolha1")
+    thread_ordena_bolha2 = threading.Thread(target=ordenar_bolha,args=(lista2,), name="OrdenaBolha2")
+ 
+    thread_ordena_pente.start()
+    thread_ordena_bolha2.start()
+    thread_ordena_bolha3.start()
+    
+ 
+    
